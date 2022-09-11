feb8d6c2b8b0eaefdbf5c6ec064842a7f624abb7
thiagosquassoni <102701107+thiagosquassoni@users.noreply.github.com>
2022-09-11 19:31:47 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,70 @@
-# Require TF version to be same as or greater than 0.12.13
+# PROVIDER
 terraform {
-  required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.36.0"
+  region = "us-east-1"
 }
 
-
-
-# Call the seed_module to build our ADO seed info
-module "bootstrap" {
-  source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
-  dynamo_db_table_name        = "aws-locks"
-  iam_user_name               = "GitHubActionsIamUser"
-  ado_iam_role_name           = "GitHubActionsIamRole"
-  aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
-  aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
+# S3 Bucket
+resource "aws_s3_bucket" "b" {
+ bucket = "aws-s3-luis-rm94405-checkpoint02"  
 }
 
-# Build the VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.1.0.0/16"
-  instance_tenancy     = "default"
-
-  tags = {
-    Name      = "Vpc"
-    Terraform = "true"
+# S3 Bucket - Config Static Website
+resource "aws_s3_bucket_website_configuration" "b-website" {
+  bucket = aws_s3_bucket.b.id
+  index_document {
+    suffix = "index.html"
   }
+    error_document {
+      key = "error.html"
+ }
 }
 
-# Build route table 1
-resource "aws_route_table" "route_table1" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "RouteTable1"
-    Terraform = "true"
-  }
+# S3 Bucket - Config ACL
+resource "aws_s3_bucket_acl" "b-acl" {
+  bucket = aws_s3_bucket.b.id
+  acl = "public-read"
 }
 
-# Build route table 2
-resource "aws_route_table" "route_table2" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "RouteTable2"
-    Terraform = "true"
-  }
+#S3 UPLOAD OBJECT
+resource "aws_s3_bucket_object" "b-error" {
+  key          = "error.html"
+  bucket       = aws_s3_bucket.b.id
+  source       = "error.html"
+  acl          = "public-read"
+  content_type = "text/html"
 }
 
+resource "aws_s3_bucket_object" "b-index" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.b.id
+  source       = "index.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+# POLICY S3
+resource "aws_s3_bucket_policy" "b-policy" {
+  bucket = aws_s3_bucket.b.id
+
+  policy      = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::aws-s3-luis-rm94405-checkpoint02/*",
+      }
+    ]
+	})
+}
